4ab92e90aec99c854b47600722b6da1f63344b50
mysterious-diamond <aaronkenn10@gmail.com>
2025-10-02 20:01:43 +0700

    Fixed some bugs, made signup and login

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::{self, Write}, error::Error};
+use std::{io::{self, Write}, error::Error, env};
+use mysql::*;
+use mysql::prelude::*;
+use dotenvy::dotenv;
 
 enum Token {
 	Help(),
@@ -7,11 +10,6 @@ enum Token {
 	List(),
 	Exit(),
 	Args(String),
-}
-
-struct Contact {
-	name: String,
-	number: String,
 }
 
 fn help() {
@@ -28,38 +26,62 @@ fn help() {
 	println!("exit => Exits the program");
 }
 
-fn add(contacts: &mut Vec<Contact>, name: String, number: String) {
-	for contact in contacts.iter_mut() {
-		if contact.name == name {
-			contact.number = number;
-			println!("Updated contact {name}'s number");
-			return;
+fn add(name: String, number: String, conn: &mut PooledConn, user_id: u32) -> Result<(), Box<dyn std::error::Error>> {
+	conn.exec_drop(
+		"INSERT INTO contacts(user_id, name, number) VALUES (:user_id, :name, :number)",
+		params! {
+			"user_id" => user_id,
+			"name" => name,
+			"number" => number
 		}
-	}
-	let new_contact = Contact {name, number};
-	contacts.push(new_contact);
+	)?;
+	
 	println!("Added contact to contact book");
+	Ok(())
 }
 
-fn list(contacts: &Vec<Contact>) {
-	for contact in contacts {
-		println!("{} : {}", contact.name, contact.number);
-	}
-
-	if contacts.is_empty() {
-		println!("No contacts in contact book");
-	}
-}
-
-fn delete(contacts: &mut Vec<Contact>, name: String) {
-	for contact in contacts.iter() {
-		if contact.name == name {
-			contacts.retain(|contact| contact.name != name);
-			println!("Removed contact {name} from contact book");
-			return;
+fn list(conn: &mut PooledConn, user_id: u32) -> Result<(), Box<dyn std::error::Error>> {
+	let query_result: Vec<(String, String)> = conn.exec(
+		"SELECT name, number FROM contacts WHERE user_id = :user_id",
+		params! {
+			"user_id" => user_id
 		}
+	)?;
+
+	if query_result.is_empty() {
+		println!("No contacts in contact book");
+		return Ok(());
 	}
-	println!("No contact named {name}");	
+	
+	for contact in query_result {
+		println!("{} : {}", contact.0, contact.1);
+	}
+
+	Ok(())
+}
+
+fn delete(conn: &mut PooledConn, contact_name: String, user_id: u32) -> Result<(), Box<dyn std::error::Error>> {
+	let query_result: Vec<u32> = conn.exec(
+		"SELECT contact_id FROM contacts WHERE name = :name AND user_id = :user_id",
+		params! {
+			"name" => &contact_name,
+			"user_id" => user_id
+		}
+	)?;
+
+	if query_result.is_empty() {
+		println!("No contact named {contact_name}");
+		return Ok(());
+	}
+
+	conn.exec_drop(
+		"DELETE FROM contacts WHERE contact_id = :contact_id",
+		params! {
+			"contact_id" => query_result[0]
+		}
+	)?;
+
+	Ok(())	
 }
 
 fn lexer(command: &str) -> Result<Vec<Token>, Box<dyn Error>> {
@@ -120,7 +142,7 @@ fn lexer(command: &str) -> Result<Vec<Token>, Box<dyn Error>> {
 }
 
 
-fn parser(tokens: &[Token], contacts: &mut Vec<Contact>) -> Result<bool, Box<dyn Error>> {
+fn parser(tokens: &[Token], conn: &mut PooledConn, user_id: u32) -> Result<bool, Box<dyn Error>> {
 	let mut index: usize = 0;
 	while index < tokens.len() {
 		let token = &tokens[index];
@@ -148,14 +170,14 @@ fn parser(tokens: &[Token], contacts: &mut Vec<Contact>) -> Result<bool, Box<dyn
 				break;
 			}
 			
-			add(contacts, name, number);
+			add(name, number, conn, user_id)?;
 		}
 		else if let Token::Delete() = token {
 			index += 1;
 			let name: String;
 			if index < tokens.len() && let Token::Args(arg) = &tokens[index] {
 				name = arg.clone();
-				delete(contacts, name);
+				delete(conn, name, user_id)?;
 				break;
 			}
 			
@@ -163,7 +185,7 @@ fn parser(tokens: &[Token], contacts: &mut Vec<Contact>) -> Result<bool, Box<dyn
 			break;
 		}
 		else if let Token::List() = token {
-			list(contacts);
+			list(conn, user_id)?;
 		}
 		else if let Token::Exit() = token {
 			return Ok(true);
@@ -175,10 +197,110 @@ fn parser(tokens: &[Token], contacts: &mut Vec<Contact>) -> Result<bool, Box<dyn
 	Ok(false)
 }
 
-fn main() {
-    println!("Contact book CLI interface, \nEnter \"help\" to see list of commands");
-    let mut contacts: Vec<Contact> = Vec::new();
+fn login(conn: &mut PooledConn) -> u32 {
+	print!("Please login first, enter your username : ");
+	io::stdout().flush().unwrap();
+	let mut username = String::new();
+	io::stdin().read_line(&mut username).unwrap();
+	let username = username.trim();
+	
+	print!("Password : ");
+	io::stdout().flush().unwrap();
+	let mut password = String::new();
+	io::stdin().read_line(&mut password).unwrap();
+	let password = password.trim();
+
+	let query_result: Vec<u32> = conn.exec(
+		"SELECT user_id FROM users WHERE username = :username AND password = :password",
+		params! {
+			"username" => username,
+			"password" => password
+		}
+	).unwrap();
+
+	if query_result.is_empty() {
+		panic!("User not valid");
+	}
+
+	query_result[0]
+}
+
+fn signup(conn: &mut PooledConn) -> Result<u32, Box<dyn std::error::Error>> {
+	let mut user_id: u32 = 0;
+	let mut username = String::new();
+	for i in 1..=3 {
+		print!("Enter a username : ");
+		io::stdout().flush()?;
+		let mut input = String::new();
+		io::stdin().read_line(&mut input)?;
+		username = input.trim().to_string();
+
+		let query_result: Vec<u32> = conn.exec(
+			"SELECT user_id FROM users WHERE username = :username",
+			params! {
+				"username" => &username
+			}
+		)?;
+
+		if query_result.is_empty() {
+			break;
+		}
+
+		if i != 3 {
+			println!("Username already in use, please try again");
+		}
+		else {
+			return Err("Unable to signup, 3 invalid signup attempts".into());
+		}
+	}
+
+	print!("Enter a password : ");
+	io::stdout().flush()?;
+	let mut password = String::new();
+	io::stdin().read_line(&mut password)?;
+	let password = password.trim();
+
+	conn.exec_drop(
+		"INSERT INTO users(username, password) VALUES (:username, :password)",
+		params! {
+			"username" => &username,
+			"password" => &password
+		}
+	)?;
+
+	user_id = conn.exec(
+		"SELECT user_id FROM users WHERE username = :username",
+		params! {
+			"username" => username
+		}
+	)?[0];
+
+	Ok(user_id)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Contact book CLI interface");
+
+    dotenv().ok();
     
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+	let pool = Pool::new(url.as_str())?;
+	let mut conn = pool.get_conn()?;
+
+	print!("Hello, enter 1 to login, anything else to sign up.");
+	io::stdout().flush().unwrap();
+	let mut choice = String::new();
+	io::stdin().read_line(&mut choice).unwrap();
+	let choice: u8 = choice.trim().parse()?;
+
+	let mut user_id: u32 = 0;
+	if choice == 1 {
+		user_id = login(&mut conn);
+	}
+	else {
+		user_id = signup(&mut conn)?;
+	}
+        
     loop {
     	print!(">> ");
     	io::stdout().flush().unwrap();
@@ -192,9 +314,11 @@ fn main() {
     	   	Err(err) => panic!("Wasn't able to lex command, {err}")
     	};
     	
-    	let exit = parser(&tokens, &mut contacts).unwrap();
+    	let exit = parser(&tokens, &mut conn, user_id).unwrap();
     	if exit {
     		break;
     	}
     }
+
+    Ok(())
 }
